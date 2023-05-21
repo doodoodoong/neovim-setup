8fde884d4470e955b3b6b3000443fce17c5cd972
doodoodoong <doo_doong@hanmail.net>
2023-05-21 21:54:49 +0900

    colorscheme deep ocean

--- lua/wise1685/core/colorscheme.lua
+++ lua/wise1685/core/colorscheme.lua
@@ -1,5 +1,6 @@
 -- set colorscheme to nightfly with protected call
 -- in case it isn't installed
+vim.g.material_style = "deep ocean"
 local status, _ = pcall(vim.cmd, "colorscheme material")
 if not status then
 	print("Colorscheme not found!") -- print error if colorscheme not installed
